aaf5fabc5374a1c3e5e41b9be019985636bb8d96
Konyaz <80565760+Konyaz@users.noreply.github.com>
2021-04-03 15:47:58 +0900

    add

---
+++ src/test/java/tests/XlslxTest.java
@@ -0,0 +1,21 @@
+
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static utils.Files.readXlsxFromPath;
+
+
+public class XlslxTest {
+    @Test
+    void xlsxTest() {
+        String xlsFilePath = "./src/test/resources/files/1.xlsx";
+        String expectedData = "123456789";
+
+        String actualData = readXlsxFromPath(xlsFilePath);
+        assertThat(actualData, containsString(expectedData));
+    }
+
+}
--- src/test/java/utils/Files.java
+++ src/test/java/utils/Files.java
@@ -1,10 +1,18 @@
+
 package utils;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Iterator;
 
 
 public class Files {
@@ -28,5 +36,51 @@ public class Files {
         return new File(path);
 
 
+    }
+    public static String readXlsxFromPath(String path){
+        String result = "";
+        XSSFWorkbook myExcelBook = null;
+
+        try {
+            myExcelBook = new XSSFWorkbook(new FileInputStream(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        XSSFSheet myExcelSheet = myExcelBook.getSheetAt(0);
+        Iterator<Row> rows = myExcelSheet.iterator();
+
+        while (rows.hasNext()) {
+            Row row = rows.next();
+            Iterator<Cell> cells = row.iterator();
+            while (cells.hasNext()) {
+                Cell cell = cells.next();
+                CellType cellType = cell.getCellType();
+                //перебираем возможные типы ячеек
+                switch (cellType) {
+//                    case Cell.CELL_TYPE_STRING:
+//                        result += cell.getStringCellValue() + "=";
+//                        break;
+//                    case Cell.CELL_TYPE_NUMERIC:
+//                        result += "[" + cell.getNumericCellValue() + "]";
+//                        break;
+//
+//                    case Cell.CELL_TYPE_FORMULA:
+//                        result += "[" + cell.getNumericCellValue() + "]";
+//                        break;
+                    default:
+                        result += cell.toString();
+                        break;
+                }
+            }
+        }
+
+        try {
+            myExcelBook.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 }
